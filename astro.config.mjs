e554c30bb7437fb68316de2b1a3f4a56988457fa
cameronapak <cameronandrewpak@gmail.com>
2025-07-15 09:03:31 -0500

    added squarespace api key to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,8 @@ export default defineConfig({
       LIBSQL_DATABASE_TOKEN: envField.string({ context: "server", access: "secret" }),
       S3_API_URL: envField.string({ context: "server", access: "secret" }),
       S3_ACCESS_KEY: envField.string({ context: "server", access: "secret" }),
-      S3_SECRET_ACCESS_KEY: envField.string({ context: "server", access: "secret" })
+      S3_SECRET_ACCESS_KEY: envField.string({ context: "server", access: "secret" }),
+      SQUARESPACE_API_KEY: envField.string({ context: "server", access: "secret" })
     }
   }
 });
